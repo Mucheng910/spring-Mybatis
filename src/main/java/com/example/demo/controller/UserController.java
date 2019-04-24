f887d6cd226e4c154a51dda83d174e1de7ca06dd
MUCHENG <3544306302>
2019-04-24 16:13:17 +0800

    first commit

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -2,11 +2,10 @@ package com.example.demo.controller;
 
 import com.example.demo.dao.UserMapper;
 import com.example.demo.entity.User;
-import com.github.pagehelper.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import com.github.pagehelper.Page;
-import com.github.pagehelper.PageHelper;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("")
@@ -14,31 +13,25 @@ public class UserController {
     @Autowired
     private UserMapper userMapper;
 
-    //http://localhost:8080/user?pageNum=2&pageSize=5
     @GetMapping("/user")
-    public Page<User> findAll( Integer pageNum, Integer pageSize) {
-        PageHelper.startPage(pageNum,pageSize);
-        Page<User> userList = userMapper.findAll();
-        return userList;
+    public List<User> findAll() {
+        return null;
     }
 
     @GetMapping("/user/{userId}")
     public User findById(@PathVariable long userId) {
-        return userMapper.findById(userId);
+        return null;
     }
 
     @PostMapping("/user")
     public void insertUser(@RequestBody User user) {
-        userMapper.insertUser(user.getName());
     }
 
     @PutMapping("/user/{userId}")
     public void updateUser(@RequestBody User user,@PathVariable int userId) {
-        userMapper.updateUser(user.getName(),userId);
     }
 
     @DeleteMapping("/user/{userId}")
     public void deleteById(@PathVariable long userId) {
-        userMapper.deleteById(userId);
     }
 }
--- src/main/java/com/example/demo/dao/UserMapper.java
+++ src/main/java/com/example/demo/dao/UserMapper.java
@@ -1,25 +1,17 @@
 package com.example.demo.dao;
 
-import com.example.demo.entity.User;
-import org.apache.ibatis.annotations.*;
 import org.springframework.stereotype.Repository;
-import com.github.pagehelper.Page;
-
 
 @Repository
 public interface UserMapper {
-    @Select("select * from user")
-    Page<User> findAll();
+    // 1. 实现查询所有用户
 
-    @Select("select * from user where id=#{id}")
-    User findById(@Param("id") long bookId);
+    // 2. 根据用户id查询用户
 
-    @Delete("delete from user where id = #{id}")
-    void deleteById(@Param("id") long id);
+    // 3. 添加新用户
 
-    @Insert("insert into user(name) values(#{name})")
-    void insertUser(@Param("name") String name);
+    // 4. 根据用户id删除用户
 
-    @Update("update user set name = #{name} where id = #{id}")
-    void updateUser(@Param("name") String name,@Param("id") int id);
+    // 5. 根据用户id更新用户
+
 }
